d8089f689ea4302891a67dd8d7793cc1cef9255a
9qhrmmmnwc-gif <9qhrmmmnwc@privaterelay.appleid.com>
2025-11-14 19:50:17 -0500

    Add files via upload

---
+++ Rayan's Portfolio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Portfolio Page 1")
+
+st.write("""
+### Team Member: Rayan Alhashim
+
+**Major:** Industrial Engineering  
+**Interests:** Technology, coding, and innovation  
+
+This is my personal portfolio page for Lab 03.  
+More content will be added in future labs!
+""")
