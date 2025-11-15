7ff281cbcded72069dc04c72c4db412e9c8efdf2
9qhrmmmnwc-gif <9qhrmmmnwc@privaterelay.appleid.com>
2025-11-14 19:22:59 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 15, Web Development - Section A")
+st.subheader("Team Members: Rayan Alhashim, Asher Punyasamudram")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **About Our Team**: A simple introductory page explaining who we are.
+2. **Portfolio Showcase**: A portfolio page showing our backgrounds and skills.
+3. **Weather API Explorer**: A page that lets you enter a city and view real-time 5-day weather forecasts with charts and tables.
+4. **Extended API Insights**: A future page for additional analysis using the same API (to be completed in later phases).
+
+""")
+
---
+++ pages/AsherPort.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Portfolio Page 2")
+
+st.write("""
+### Team Member: Asher Punyasamudram
+
+**Major:** Computer Science 
+**Interests:** Soccer, Coding, Food
+
+This is my personal portfolio page for Lab 03, showcasing my background and skills.
+""")
